a7c8fc1c17c97f29915d927a2be1ac123e7387e3
Victor <victorde.ac@gmail.com>
2022-05-13 22:25:53 -0300

    fix check_permutation and add complexity analysis

--- arrays_and_lists/check_permutation/__init__.py
+++ arrays_and_lists/check_permutation/__init__.py
@@ -1,8 +1,23 @@
 def check_permutation(string_1: str, string_2: str) -> bool:
-    return set(string_1) == set(string_2) and len(string_1) == len(string_2)
+    """Time O(n) Space O(n)"""
+    counts = {}
+    for w in string_1:
+        counts[w] = counts.get(w, 0) + 1
 
+    for w in string_2:
+        if w in counts:
+            counts[w] = counts[w] - 1
+        else:
+            return False
+        if counts[w] == 0:
+            del counts[w]
+    
+    if len(counts) == 0:
+        return True
+    return False
 
 def check_permutation_brute(string_1: str, string_2: str) -> bool:
+    """Time O(n^2) Space O(1)"""
     if len(string_1) != len(string_2): 
         return False
     for ch in string_1:
